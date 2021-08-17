dbd23b2c582d8e71fd01dfc94251f01b4bcd56bb
IT21040276 <87693535+IT21040276@users.noreply.github.com>
2021-08-17 18:14:58 +0000

    Completed

--- tute04.cpp
+++ tute04.cpp
@@ -1,4 +1,4 @@
-/*Exercise 4 - Functions
+ /*Exercise 4 - Functions
 
 Write a program to calculate the function called nCr which is defined as
 
@@ -18,12 +18,21 @@ long nCr(int n, int r);
 
 int main() {
   int n, r;
-  std::cout << "Enter a value for n ";
+  std::cout << "Enter a value for n = ";
   std::cin >> n;
-  std::cout << "Enter a value for r ";
+  std::cout << "Enter a value for r =  ";
   std::cin >> r;
   std::cout << "nCr = ";
   std::cout << nCr(n,r);
   std::cout << std::endl;
   return 0;
+}
+long Factorial(int no)
+{
+ return ( no != 0 ? no * Factorial(no-1) : 1);
+}
+ 
+long nCr(int n, int r)
+{
+ return (Factorial(n)/( Factorial(r) * Factorial(n-r)));
 }
